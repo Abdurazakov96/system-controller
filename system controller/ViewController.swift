22c3ab3a927b55c6162b7c56b41e9542d7e72136
Muhammad <abdurazakov96@mail.ru>
2019-08-17 17:10:23 +0300

    Initial Commit

---
+++ system controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  system controller
+//
+//  Created by Магомед Абдуразаков on 17/08/2019.
+//  Copyright © 2019 Магомед Абдуразаков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
